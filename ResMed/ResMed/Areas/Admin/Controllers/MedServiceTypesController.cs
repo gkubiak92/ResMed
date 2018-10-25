fec3583ccff721c6f06b9e9488db25bcbac0e066
gkubiak92 <43858788+gkubiak92@users.noreply.github.com>
2018-10-25 19:35:15 +0200

    Initial

---
+++ ResMed/ResMed/Areas/Admin/Controllers/MedServiceTypesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ResMed.Data;
+
+namespace ResMed.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MedServiceTypesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public MedServiceTypesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.MedServiceTypes.ToList());
+        }
+    }
+}
---
+++ ResMed/ResMed/Data/Migrations/20181025172307_InitialWithMedServiceTypes.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ResMed.Data.Migrations
+{
+    public partial class InitialWithMedServiceTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MedServiceTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MedServiceTypes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MedServiceTypes");
+        }
+    }
+}
